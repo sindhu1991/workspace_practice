9a6c820527fe421168d3fc4734bfb144afcd716e
sindhumallipeddy <sindhumallipeddy@gmail.com>
2018-11-14 01:56:17 +0000

    Added actionmailer settings in env

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'sindhumallipeddy@gmail.com'
-    
+  default to: 'seshaui@gmail.com'
+  
   def contact_email(name, email, body)
-      @name = name
-      @email = email
-      @body = body
-      mail(from: email, subject: 'Contact Form Message')
-   end
-   
-end 
+    @name = name
+    @email = email
+    @body = body
+    
+    mail(from: email, subject: 'Contact Form Message')
+  end
+end
