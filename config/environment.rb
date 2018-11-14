0fd8632bee81c1aa1dfd507591f0e4e79fcfbe82
sindhumallipeddy <sindhumallipeddy@gmail.com>
2018-11-14 02:48:20 +0000

    Added actionmailer settings in env

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+require "resolv-replace.rb"
 
 # Initialize the Rails application.
 Rails.application.initialize!
