f7a35ad92cfcf4c81fda0a035ce66cf07b3a4afb
sindhumallipeddy <sindhumallipeddy@gmail.com>
2018-11-14 02:51:42 +0000

    Added actionmailer settings in env

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 
 
 ActionMailer::Base.smtp_settings = {
-    :address => 'stmp.sendgrid.net',
+    :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
